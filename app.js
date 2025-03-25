1bf0efdd172e9b6b082f062f380956532691e718
sumit <sumitrai3252@gmail.com>
2025-03-25 18:35:48 +0530

    Add Prompt Api

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const Groq = require("groq-sdk");
+require("dotenv").config();
 const PORT = 3000;
 
-app.get('/hello', (req, res, next) => {
-    res.status(200).json({
-        msg: 'Hello World'
-    });
+const client = new Groq({
+  apiKey: process.env["GROQ_API_KEY"], // This is the default and can be omitted
+});
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.send("Hello Sumit Here ");
+});
+
+app.post("/prompt", async (req, res, next) => {
+  const prompt = req.body.prompt;
+  const chatCompletion = await client.chat.completions.create({
+    messages: [{ role: "user", content: prompt }],
+    model: "llama3-8b-8192",
+  });
+
+  res.status(200).json({
+    response: chatCompletion.choices[0].message.content,
+  });
 });
 
 app.listen(PORT, () => {
-    console.log('Express server started on http://localhost:3000');
+  console.log("Express server started on http://localhost:3000");
 });
